a04858e96cb845b039b488e6f0918a16ff0a0e10
Anderson Sant Anna <anderson.santanna14@gmail.com>
2020-09-26 19:16:52 -0300

    created card custom and finish home scene

--- lib/scenes/Home.dart
+++ lib/scenes/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScene extends StatelessWidget {
   const HomeScene({Key key}) : super(key: key);
@@ -6,16 +7,83 @@ class HomeScene extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 500,
       color: Colors.green[800],
-      // width: ;,
+      padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Text(
-            "Noticias Ecologicas",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+          Container(
+            child: Text(
+              "Noticias Ecologicas",
+              style: GoogleFonts.caveat(
+                color: Colors.white,
+                textStyle: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+              ),
+            ),
           ),
-          // Container()
+          Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 30.0),
+              height: 320.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  CardCustom(),
+                  CardCustom(),
+                  CardCustom(),
+                ],
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class CardCustom extends StatelessWidget {
+  const CardCustom({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 320,
+      margin: EdgeInsets.only(right: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.black,
+        image: DecorationImage(
+          image: AssetImage("assets/images/monkey.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 25, left: 10, right: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 7),
+              child: Text(
+                "Brazil",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 35,
+                ),
+              ),
+            ),
+            Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
